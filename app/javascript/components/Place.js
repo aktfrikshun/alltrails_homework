d5e4c86b1f5606a0c75e5d65a9429d8cb8d35fea
Allen Taylor <akt@frikshun.com>
2022-06-10 19:55:40 -0400

    Add font-awesome

--- app/javascript/components/Place.js
+++ app/javascript/components/Place.js
@@ -1,11 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
 class Place extends React.Component {
+
   render () {
+    var image_src = this.props.place.icon;
+    console.log(this.props.place);
     return (
       <div class="place-item">
-       <h2>{this.props.place.name}</h2>
-       {this.props.place.formatted_address}
+        <em><strong>{this.props.place.name}</strong></em><br/>
+        Rating: {this.props.place.rating}<br/>
+        Price: {this.props.place.price_level}
       </div>
     );
   }
